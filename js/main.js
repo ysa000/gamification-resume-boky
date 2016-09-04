ecf163d71486d4a942b2f6277042f5ed12ea23b3
Ysa <isabelle.yim@gmail.com>
2016-09-04 21:27:31 +0200

    random

--- js/main.js
+++ js/main.js
@@ -366,7 +366,7 @@
         }
 
         function detectCollide(obj1, obj2) {
-            if (obj1.x < obj2.x && // posX Boky < posX Villain + width Villain
+            if (obj1.x < obj2.x &&
                 obj1.x + obj1.width > obj2.x + obj2.width &&
                 obj1.y < obj2.y + obj2.height &&
                 obj1.height + obj1.y > obj2.y) {
